64a32390cb40729de280909d31b204d5fd70b8b1
440H1 <aadhinaaraayanan.msp@gmail.com>
2026-08-07 18:56:33 +0530

    The Uniue Letter Hunt Mini-Game (completed)

---
+++ src/UniqueLetterHunt.java
@@ -0,0 +1,36 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class UniqueLetterHunt {
+
+    public static char findFirstNonRepeatingChar(String text) {
+        Map<Character, Integer> freq = new HashMap<>();
+        for (char c : text.toCharArray()) {
+            freq.put(c, freq.getOrDefault(c, 0) + 1);
+        }
+
+        for (char c : text.toCharArray()) {
+            if (freq.get(c) == 1) {
+                return c;
+            }
+        }
+
+        return '\0';
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a word or sentence: ");
+        String input = sc.nextLine();
+
+        char result = findFirstNonRepeatingChar(input);
+
+        if (result == '\0') {
+            System.out.println("No Non-Repeating Character Found");
+        } else {
+            System.out.println("First Non-Repeating Character: '" + result + "'");
+        }
+    }
+}
+
